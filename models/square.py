d493ffcc9c36c0060b197fc1ecfc1e743231d389
Kevin7744 <arapbiisubmissions@gmail.com>
2023-11-13 22:54:36 -0800

    Square size

--- models/square.py
+++ models/square.py
@@ -9,3 +9,22 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0,id=None):
         """ Initializes an instance of class Square """
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ Returns a string representation of the instance """
+        a = self.id
+        b = self.width
+        c = self.x 
+        d = self.y
+        return "[Square] ({}) {}/{} - {}".format(a, c, d, b)
+
+    @property
+    def size(self):
+        """ retrieves the attribute size """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ Validate and sets the attribut size """
+        self.width = value
+        self.heght = value
